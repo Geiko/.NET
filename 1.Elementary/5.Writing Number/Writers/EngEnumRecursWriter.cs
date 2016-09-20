2636c3b7bf1a0d6aee43d8571f7b2ad26a735254
Geiko <k.i.geiko@gmail.com>
2016-09-20 21:44:25 +0300

    numberToText

---
+++ 1.Elementary/5.Writing Number/Writers/EngEnumRecursWriter.cs
@@ -0,0 +1,322 @@
+﻿//-----------------------------------------------------------------------
+// <copyright file="EngEnumRecursWriter.cs" company="SoftServe">
+//     Copyright (c) SoftServe. All rights reserved.
+// </copyright>
+// <author>Kostiantyn Geiko</author>
+//-----------------------------------------------------------------------
+namespace _5.Writing_Number.Writers
+{
+    using System;
+    using System.Text;
+    using Properties;
+
+    /// <summary>
+    /// Class to convert numbers to english language with recursion.
+    /// </summary>
+    public class EngEnumRecursWriter : BaseNumberWriter
+    {
+        /// <summary>
+        /// Initializes a new instance of the 
+        /// <see cref="EngEnumRecursWriter" /> 
+        /// class that shows specification.
+        /// </summary>
+        public EngEnumRecursWriter() : base()
+        {
+        }
+
+        /// <summary>
+        /// Initializes a new instance of the 
+        /// <see cref="EngEnumRecursWriter" /> class with one parameter.
+        /// </summary>
+        /// <param name="arg">Number as String 
+        /// that is converted to words.</param>
+        /// <param name="maxValue">Maximum value of number that 
+        /// is to be converted to text.</param>
+        public EngEnumRecursWriter(string arg, bool maxValue)
+            : base(arg, maxValue)
+        {
+        }
+
+        /// <summary>
+        /// Enumeration of english counting words
+        /// </summary>
+        public enum Numbers
+        {
+            /// <summary>
+            /// Represents minus.
+            /// </summary>
+            minus = -1,
+
+            /// <summary>
+            /// Represents zero.
+            /// </summary>
+            zero,
+
+            /// <summary>
+            /// Represents one.
+            /// </summary>
+            one,
+
+            /// <summary>
+            /// Represents two.
+            /// </summary>
+            two,
+
+            /// <summary>
+            /// Represents three.
+            /// </summary>
+            three,
+
+            /// <summary>
+            /// Represents four.
+            /// </summary>
+            four,
+
+            /// <summary>
+            /// Represents five.
+            /// </summary>
+            five,
+
+            /// <summary>
+            /// Represents six.
+            /// </summary>
+            six,
+
+            /// <summary>
+            /// Represents seven.
+            /// </summary>
+            seven,
+
+            /// <summary>
+            /// Represents eight.
+            /// </summary>
+            eight,
+
+            /// <summary>
+            /// Represents nine.
+            /// </summary>
+            nine,
+
+            /// <summary>
+            /// Represents ten.
+            /// </summary>
+            ten,
+
+            /// <summary>
+            /// Represents eleven.
+            /// </summary>
+            eleven,
+
+            /// <summary>
+            /// Represents twelve.
+            /// </summary>
+            twelve,
+
+            /// <summary>
+            /// Represents thirteen.
+            /// </summary>
+            thirteen,
+
+            /// <summary>
+            /// Represents fourteen.
+            /// </summary>
+            fourteen,
+
+            /// <summary>
+            /// Represents fifteen.
+            /// </summary>
+            fifteen,
+
+            /// <summary>
+            /// Represents sixteen.
+            /// </summary>
+            sixteen,
+
+            /// <summary>
+            /// Represents seventeen.
+            /// </summary>
+            seventeen,
+
+            /// <summary>
+            /// Represents eighteen.
+            /// </summary>
+            eighteen,
+
+            /// <summary>
+            /// Represents nineteen.
+            /// </summary>
+            nineteen,
+
+            /// <summary>
+            /// Represents twenty.
+            /// </summary>
+            twenty,
+
+            /// <summary>
+            /// Represents thirty.
+            /// </summary>
+            thirty = 30,
+
+            /// <summary>
+            /// Represents forty.
+            /// </summary>
+            forty = 40,
+
+            /// <summary>
+            /// Represents fifty.
+            /// </summary>
+            fifty = 50,
+
+            /// <summary>
+            /// Represents sixty.
+            /// </summary>
+            sixty = 60,
+
+            /// <summary>
+            /// Represents seventy.
+            /// </summary>
+            seventy = 70,
+
+            /// <summary>
+            /// Represents eighty.
+            /// </summary>
+            eighty = 80,
+
+            /// <summary>
+            /// Represents ninety.
+            /// </summary>
+            ninety = 90,
+
+            /// <summary>
+            /// Represents hundred.
+            /// </summary>
+            hundred = 100,
+
+            /// <summary>
+            /// Represents thousand.
+            /// </summary>
+            thousand = 1000,
+
+            /// <summary>
+            /// Represents million.
+            /// </summary>
+            million = 1000000,
+
+            /// <summary>
+            /// Represents milliard.
+            /// </summary>
+            milliard = 1000000000
+        }
+
+        /// <summary>
+        /// Main method for converting number to words.
+        /// </summary>
+        /// <returns>Resulted string</returns>
+        public override string ConvertToText()
+        {
+            this.MaxNumber = int.MaxValue;
+            return this.NumberToWords(this.NumberToConvert);
+        }
+
+        /// <summary>
+        /// Recursion method to convert number to text.
+        /// </summary>
+        /// <param name="number">Number to be converted to text.</param>
+        /// <returns>Converted text.</returns>
+        private string NumberToWords(int number)
+        {
+            if (number == 0)
+            {
+                return ((Numbers)0).ToString();
+            }
+
+            StringBuilder builder = new StringBuilder();
+            if (number < 0)
+            {
+                builder.Append(((Numbers)(-1)).ToString());
+                builder.Append(Resources.Blank);
+                builder.Append(this.NumberToWords(Math.Abs(number)));
+                return builder.ToString();
+            }
+
+            string words = string.Empty;
+
+            if ((number / 1000000000) > 0)
+            {
+                builder.Append(words);
+                builder.Append(this.NumberToWords(number / 1000000000));
+                builder.Append(Resources.Blank);
+                builder.Append(((Numbers)1000000000).ToString());
+                builder.Append(Resources.Blank);
+                words = builder.ToString();
+                number %= 1000000000;
+            }
+
+            if ((number / 1000000) > 0)
+            {
+                builder.Append(this.NumberToWords(number / 1000000));
+                builder.Append(Resources.Blank);
+                builder.Append(((Numbers)1000000).ToString());
+                builder.Append(Resources.Blank);
+                words = builder.ToString();
+                number %= 1000000;
+            }
+
+            if ((number / 1000) > 0)
+            {
+                builder.Append(this.NumberToWords(number / 1000));
+                builder.Append(Resources.Blank);
+                builder.Append(((Numbers)1000).ToString());
+                builder.Append(Resources.Blank);
+                words = builder.ToString();
+                number %= 1000;
+            }
+
+            if ((number / 100) > 0)
+            {
+                builder.Append(this.NumberToWords(number / 100));
+                builder.Append(Resources.Blank);
+                builder.Append(((Numbers)100).ToString());
+                builder.Append(Resources.Blank);
+                words = builder.ToString();
+                number %= 100;
+            }
+
+            if (number > 0)
+            {
+                if (words != string.Empty)
+                {
+                    words = string.Format(
+                            Resources.DisplayTwo,
+                            words,
+                            Resources.And);
+                }
+
+                if (number < 20)
+                {
+                    words = string.Format(
+                            Resources.DisplayTwo,
+                            words,
+                            ((Numbers)number).ToString());
+                }
+                else
+                {
+                    words = string.Format(
+                            Resources.DisplayTwo,
+                            words,
+                            ((Numbers)(number - (number % 10))).ToString());
+                    if ((number % 10) > 0)
+                    {
+                        words = string.Format(
+                            Resources.DisplayThree,
+                            words,
+                            Resources.Hyphen,
+                            ((Numbers)(number % 10)).ToString());
+                    }
+                }
+            }
+
+            return words;
+        }
+    }
+}
---
+++ 1.Elementary/5.Writing Number/Writers/RusEnumWriter.cs
@@ -0,0 +1,346 @@
+﻿//-----------------------------------------------------------------------
+// <copyright file="RusEnumWriter.cs" company="SoftServe">
+//     Copyright (c) SoftServe. All rights reserved.
+// </copyright>
+// <author>Kostiantyn Geiko</author>
+//-----------------------------------------------------------------------
+namespace _5.Writing_Number
+{
+    using System;
+    using Properties;
+
+    /// <summary>
+    /// Class to convert numbers to russian language.
+    /// </summary>
+    public class RusEnumWriter : BaseNumberWriter
+    {
+        /// <summary>
+        /// Initializes a new instance of the 
+        /// <see cref="RusEnumWriter" /> class.
+        /// </summary>
+        public RusEnumWriter() : base()
+        {
+        }
+
+        /// <summary>
+        /// Initializes a new instance of the 
+        /// <see cref="RusEnumWriter" /> class with one parameter.
+        /// </summary>
+        /// <param name="arg">Number as String 
+        /// that is converted to words.</param>
+        public RusEnumWriter(string arg) : base(arg)
+        {
+        }
+
+        /// <summary>
+        /// Types of russian counting words.
+        /// </summary>
+        public enum Numbers
+        {
+            /// <summary>
+            /// Represents minus.
+            /// </summary>
+            минус = -1,
+
+            /// <summary>
+            /// Represents zero.
+            /// </summary>
+            ноль,
+
+            /// <summary>
+            /// Represents one.
+            /// </summary>
+            один,
+
+            /// <summary>
+            /// Represents two.
+            /// </summary>
+            два,
+
+            /// <summary>
+            /// Represents three.
+            /// </summary>
+            три,
+
+            /// <summary>
+            /// Represents four.
+            /// </summary>
+            четыре,
+
+            /// <summary>
+            /// Represents five.
+            /// </summary>
+            пять,
+
+            /// <summary>
+            /// Represents six.
+            /// </summary>
+            шесть,
+
+            /// <summary>
+            /// Represents seven.
+            /// </summary>
+            семь,
+
+            /// <summary>
+            /// Represents eight.
+            /// </summary>
+            восемь,
+
+            /// <summary>
+            /// Represents nine.
+            /// </summary>
+            девять,
+
+            /// <summary>
+            /// Represents ten.
+            /// </summary>
+            десять,
+
+            /// <summary>
+            /// Represents eleven.
+            /// </summary>
+            одиннадцать,
+
+            /// <summary>
+            /// Represents twelve.
+            /// </summary>
+            двенадцать,
+
+            /// <summary>
+            /// Represents thirteen.
+            /// </summary>
+            тринадцать,
+
+            /// <summary>
+            /// Represents fourteen.
+            /// </summary>
+            четырнадцать,
+
+            /// <summary>
+            /// Represents fifteen.
+            /// </summary>
+            пятнадцать,
+
+            /// <summary>
+            /// Represents sixteen.
+            /// </summary>
+            шестнадцать,
+
+            /// <summary>
+            /// Represents seventeen.
+            /// </summary>
+            семнадцать,
+
+            /// <summary>
+            /// Represents eighteen.
+            /// </summary>
+            восемнадцать,
+
+            /// <summary>
+            /// Represents nineteen.
+            /// </summary>
+            девятнадцать,
+
+            /// <summary>
+            /// Represents twenty.
+            /// </summary>
+            двадцать,
+
+            /// <summary>
+            /// Represents thirty.
+            /// </summary>
+            тридцать = 30,
+
+            /// <summary>
+            /// Represents forty.
+            /// </summary>
+            сорoк = 40,
+
+            /// <summary>
+            /// Represents fifty.
+            /// </summary>
+            пятьдесят = 50,
+
+            /// <summary>
+            /// Represents sixty.
+            /// </summary>
+            шестьдесят = 60,
+
+            /// <summary>
+            /// Represents seventy.
+            /// </summary>
+            семьдесят = 70,
+
+            /// <summary>
+            /// Represents eighty.
+            /// </summary>
+            восемьдесят = 80,
+
+            /// <summary>
+            /// Represents ninety.
+            /// </summary>
+            девяносто = 90,
+
+            /// <summary>
+            /// Represents one hundred.
+            /// </summary>
+            сто = 100,
+
+            /// <summary>
+            /// Represents two hundred.
+            /// </summary>
+            двести = 200,
+
+            /// <summary>
+            /// Represents three hundred.
+            /// </summary>
+            триста = 300,
+
+            /// <summary>
+            /// Represents four hundred.
+            /// </summary>
+            четыреста = 400,
+
+            /// <summary>
+            /// Represents five hundred.
+            /// </summary>
+            пятьсот = 500,
+
+            /// <summary>
+            /// Represents six hundred.
+            /// </summary>
+            шестьсот = 600,
+
+            /// <summary>
+            /// Represents seven hundred.
+            /// </summary>
+            семьсот = 700,
+
+            /// <summary>
+            /// Represents eight hundred.
+            /// </summary>
+            восемьсот = 800,
+
+            /// <summary>
+            /// Represents nine hundred.
+            /// </summary>
+            девятьсот = 900
+        }
+
+        /// <summary>
+        /// Main method for converting number to words.
+        /// </summary>
+        /// <returns>Resulted string</returns>
+        public override string ConvertToText()
+        {
+            bool minus = this.NumberToConvert < 0 ? true : false;
+            this.NumberToConvert = Math.Abs(this.NumberToConvert);
+            string result = string.Empty;
+            if (this.NumberToConvert <= 20)
+            {
+                result = string.Format(
+                    Resources.Display,
+                    ((Numbers)this.NumberToConvert).ToString());
+            }
+            else if (this.NumberToConvert > 20 && this.NumberToConvert < 100)
+            {
+                result = this.GetResultFor21To99();
+            }
+            else if (this.NumberToConvert >= 100)
+            {
+                result = this.GetResultForMore99();
+            }
+
+            if (minus)
+            {
+                result = string.Format(
+                        Resources.DisplayTwo,
+                        ((Numbers)(-1)).ToString(),
+                        result);
+            }
+
+            return result;
+        }
+
+        /// <summary>
+        /// Definition a string for a range 21 - 99.
+        /// </summary>
+        /// <returns>Resulted string.</returns>
+        private string GetResultFor21To99()
+        {
+            int a2 = this.NumberToConvert % 10;
+            int a1 = this.NumberToConvert - a2;
+
+            if (a2 == 0)
+            {
+                return string.Format(
+                    Resources.Display,
+                    ((Numbers)a1).ToString());
+            }
+            else
+            {
+                return string.Format(
+                    Resources.DisplayTwo,
+                    ((Numbers)a1).ToString(),
+                    ((Numbers)a2).ToString());
+            }
+        }
+
+        /// <summary>
+        /// Definition a string for a number > 99.
+        /// </summary>
+        /// <returns>Resulted string.</returns>
+        private string GetResultForMore99()
+        {
+            int a3 = this.NumberToConvert % 10;
+            int a2 = (this.NumberToConvert - a3) % 100;
+            int a22 = this.NumberToConvert % 100;
+            int a1 = this.NumberToConvert - a2 - a3;
+
+            if (a3 == 0 && a2 == 0)
+            {
+                return string.Format(
+                    Resources.Display,
+                    ((Numbers)a1).ToString());
+            }
+            else if (a3 == 0 && a2 != 0)
+            {
+                return string.Format(
+                    Resources.DisplayTwo,
+                    ((Numbers)a1).ToString(),
+                    ((Numbers)a2).ToString());
+            }
+            else if (a3 != 0 && a2 == 0)
+            {
+                return string.Format(
+                    Resources.DisplayTwo,
+                    ((Numbers)a1).ToString(),
+                    ((Numbers)a3).ToString());
+            }
+            else if (a3 != 0 && a2 != 0 && a1 != 0)
+            {
+                return string.Format(
+                    Resources.DisplayThree,
+                    ((Numbers)a1).ToString(),
+                    ((Numbers)a2).ToString(),
+                    ((Numbers)a3).ToString());
+            }
+            else if (a22 < 21 && a22 != 0)
+            {
+                return string.Format(
+                    Resources.DisplayTwo,
+                    ((Numbers)a1).ToString(),
+                    ((Numbers)a22).ToString());
+            }
+            else if (a22 == 0)
+            {
+                return string.Format(
+                    Resources.Display,
+                    ((Numbers)a1).ToString());
+            }
+
+            return null;
+        }
+    }
+}
