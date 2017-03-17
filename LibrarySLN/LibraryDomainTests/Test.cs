bb76d8b6ad69fad5d217096386eacd687c89533f
Geiko <k.i.geiko@gmail.com>
2017-03-17 19:18:47 +0200

    Initial commit; Projects structure was created

---
+++ LibrarySLN/LibraryDomainTests/Test.cs
@@ -0,0 +1,20 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Library;
+
+namespace LibraryDomainTests
+{
+    [TestFixture]
+    public class Test
+    {
+        [Test]
+        public void FirstTest()
+        {
+            //Library.Ragistrator
+        }
+    }
+}
